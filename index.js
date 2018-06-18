bc41c7314a73f5ef955656812551d59b3ff3e669
DrMady <1933.hi@gmail.com>
2018-06-18 14:30:12 +0200

    I'm not a wizard

--- index.js
+++ index.js
@@ -2,7 +2,29 @@ var hp = $.get("http://hp-api.herokuapp.com/api/characters")
 .then(function(data){
 data.forEach(function(characters){
 
-$('.list-group').append('<li id="changer" class="list-group-item active">'+characters.name+'</li>'+'<li class="list-group-item"> <p>House:</p>'+characters.house+'</li>');
+// Unavailable feature too lazy to fix it
+
+/*if (characters.house == "Gryffindor") {
+	$('#house').addClass('.Gryffindor');
+}
+
+else if (characters.house == "Slytherin") {
+	$('#house').addClass('.Slytherin');
+}
+
+else if (characters.house == "Ravenclaw") {
+	$('#house').addClass('.Ravenclaw');
+}
+
+else if (characters.house == "Hufflepuff") {
+	$('#house').addClass('.Hufflepuff');
+}
+
+else {
+	$('p').append('None');
+}*/
+
+$('.list-group').append('<li id="changer" class="list-group-item active">'+characters.name+'</li>'+'<li id="house" class="list-group-item"> <p>House:</p>'+characters.house+'</li>');
 
 
 
